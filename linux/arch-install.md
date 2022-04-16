928f328296999af46062828a26892d3a2db19525
henuhpf <1728290896@qq.com>
2022-04-16 16:21:23 +0800

    feat: 添加arch安装博客地址

---
+++ linux/arch-install.md
@@ -0,0 +1,8 @@
+https://archlinuxstudio.github.io/ArchLinuxTutorial
+
+https://wiki.archlinux.org
+
+https://github.com/LucasWang474/ArcoLinux-Configurations
+
+# wm wiki
+https://awesomewm.org/
--- modRemote.sh
+++ modRemote.sh
@@ -3,7 +3,9 @@ git_username=$(git config user.name)
 repository_name=$(basename "$PWD")
 
 git remote remove origin
-git remote add github git@github.com:$git_username/$repository_name.git
-git remote add gitee git@gitee.com:$git_username/$repository_name.git
+# git remote add github git@github.com:$git_username/$repository_name.git
+# git remote add gitee git@gitee.com:$git_username/$repository_name.git
+git remote add github https://github.com/$git_username/$repository_name.git
+git remote add gitee https://gitee.com/$git_username/$repository_name.git
 
 echo "`git remote -v`"
